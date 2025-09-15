43bf699c51f9a4e15245f10476d4c947cac93ef0
bjchris32 <48957864+bjchris32@users.noreply.github.com>
2025-09-15 17:01:49 -0500

    feat: use anyhow to trace the full error while showing elegant error message

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use std::io::BufRead;
+use anyhow::{Context, Result};
 
 #[derive(Parser)]
 struct Cli {
@@ -12,7 +13,8 @@ struct Cli {
 // Box<dyn Error> : “Box is a heap pointer to some type that implements Error, but we don’t know its size or exact type at compile time.
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
-    let file = std::fs::File::open(&args.path)?;
+    let file = std::fs::File::open(&args.path)
+        .with_context(|| format!("could not read file `{}`", &args.path.display()))?;
     let reader = std::io::BufReader::new(file);
     for line_result in reader.lines() {
         let line = line_result?;
