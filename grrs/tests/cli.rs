719dae1f8c2b1b15de47d6a0aa9ba0e77f6b8093
Chris Shao <bjchris32@gmail.com>
2026-01-22 17:00:58 -0600

    test: add file not exist test case

--- grrs/tests/cli.rs
+++ grrs/tests/cli.rs
@@ -1,5 +1,19 @@
 use grrs::*;
 use indicatif::ProgressBar;
+use assert_cmd::cargo::*; // Import cargo_bin_cmd! macro and methods
+use predicates::prelude::*; // Used for writing assertions
+
+#[test]
+fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = cargo_bin_cmd!("grrs");
+
+    cmd.arg("foobar").arg("test/file/doesnt/exist");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("could not read file"));
+
+    Ok(())
+}
 
 #[test]
 fn test_find_match() {
